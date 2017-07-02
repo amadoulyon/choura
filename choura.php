f3a21ce5b1a0885e7de667966733aff0d44af4da
Amadou <amadoulyon@gmail.com>
2017-07-03 00:42:03 +0200

    Construction du message à envoyer ajouté

--- choura.php
+++ choura.php
@@ -8,7 +8,7 @@ $emir = $_POST['emir'];
 $participant = $_POST['participant'];
 $nomAdeb = $_POST['nomAdeb'];
 $adeb = $_POST['adeb'];
-$id_infos = 1;
+
 
 
 $req = $bdd->exec("DELETE FROM infos");
@@ -34,5 +34,56 @@ while($donnees = $reponse->fetch())
 }
 
 echo 'Le jeu a bien été ajouté';
+
+$mail = 'amadoulyon@gmail.com';// Déclaration de l'adresse de destination
+if(!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail))//On filtre les serveurs qui rencontrent des bugs.
+{
+	$passage_ligne = "\r\n";
+}
+else
+{
+	$passage_ligne = "\n";
+}
+//==========Déclaration des messages au format texte et au format HTML
+$message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
+$message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par <i>script PHP</i>.</body></html>";
+//=========================
+
+//==========Création de la boundary
+$boundary = "-----=".md5(rand());
+//==========
+
+//==========Définition du sujet.
+$sujet = "Hey mon ami";
+//==========
+
+//==========Création du header de l'e-mail.
+$header = "From: \"BARRY\"<amadoulyon@gmail.com>".$passage_ligne;
+$header.= "Reply-to: \"BARRY\"<amadoulyon@gmail.com>".$passage_ligne;
+$header.= "MINE-Version: 1.0".$passage_ligne;
+$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+//==========
+
+//==========Création du message.
+$message = $passage_ligne."--".$boundary.$passage_ligne;
+//==========Ajout du message au format texte.
+$message .= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+$message .= "Content-Transfert-Encoding: 8Bit".$passage_ligne;
+$message .= $passage_ligne.$message_txt.$passage_ligne;
+//==========
+$message.= $passage_ligne."--".$boundary.$passage_ligne;
+//Ajout du message au format HTML
+$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_html.$passage_ligne;
+//==========
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+//==========
+
+//==========Envoie de l'e-mail.
+mail($mail,$sujet,$message,$header);
+//==========
+
 
  ?>
